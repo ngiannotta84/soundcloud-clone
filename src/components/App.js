3cf51002e30fda53b17a06c4ffffe5dde24a7fdf
Perry Baran <perrybaran@gmail.com>
2022-12-21 13:12:31 +0000

    logged in users are saved

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../styles/App.css";
+import Cookie from "js-cookie";
+import jwtDecode from "jwt-decode";
 import Navbar from "./Navbar";
 import Home from "./Home";
 import Login from "./Login";
@@ -10,18 +12,40 @@ import Profile from "./Profile";
 import SignUp from "./SignUp";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: null,
+    id: null,
+  });
+
+  const handleLogin = (data) => {
+    setUser({
+      name: data.name,
+      id: data.id,
+    });
+  };
+
+  useEffect(() => {
+    const token = Cookie.get("userToken");
+    if (token) {
+      const currentUser = jwtDecode(token);
+      handleLogin(currentUser);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <h2>SoundClone</h2>
       <Router>
-        <Navbar />
+        <Navbar userName={user.name} />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login handleLogin={handleLogin} />} />
           <Route path="/search" element={<Search />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/profile/:userName" element={<Profile />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/signup"
+            element={<SignUp handleLogin={handleLogin} />}
+          />
         </Routes>
       </Router>
     </div>
