0bcc9bd5f87d318996360ec528bb9d0d58e192b5
nicola giannotta <nicolagiannotta84@gmail.com>
2022-12-22 16:16:44 +0000

    upload.js amended

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -3,32 +3,38 @@ import { React, useState } from "react";
 const Upload = () => {
   const [album, setAlbum] = useState("");
   const handleAlbumNameChange = (e) => {
-    setAlbum((prev) => ({
-      ...prev,
-      albumName: e.target.value,
-    }));
+    setAlbum(e.target.value);
   };
-  const [Image, setImage] = useState("");
+  const [image, setImage] = useState("");
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
   };
-  const [songs, setSongs] = useState([]);
-  const handleSongsChange = (e) => {
-    setSongs(e.target.files);
+  const emptysong = {
+    songName: "",
+    audioFile: "",
   };
-  const uploadAlbum = (e) => {
-    e.preventDefault();
-    const initialState = {
-      albumName: album,
-      imageFile: Image,
-      Songs: songs.map((song) => ({
-        songName: song.name,
-        audioFile: song,
-      })),
-    };
-    console.log(initialState);
+  const [songs, setSongs] = useState([emptysong]);
+  const addSong = () => {
+    setSongs((prev) => [...prev, emptysong]);
+  };
+  const handleSongsChange = (e) => {
+    setSongs(e.target.files[0]);
   };
 
+  const uploadAlbum = (e) => {
+    e.preventDefault();
+    const FinalState = {
+      albumName: album,
+      imageFile: image,
+      Songs: [
+        {
+          songName: songs.name,
+          audioFile: songs,
+        },
+      ],
+    };
+    return FinalState;
+  };
   return (
     <div>
       <span>Upload You Album</span>
@@ -69,6 +75,15 @@ const Upload = () => {
               onChange={handleSongsChange}
             />
           </label>
+          {songs.map((song, index) => {
+            return (
+            
+
+             )
+          })}
+          <button className="submit-button" type="submit" onClick={addSong}>
+            Add Song
+          </button>
           <button className="submit-button" type="submit">
             Submit
           </button>
