28c179e6b3deb1fd2d5d5d4203323d079ed557e9
Perry Baran <perrybaran@gmail.com>
2022-12-23 15:49:21 +0000

    added getSongs request

---
+++ src/requests/getSongs.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+import axios from "axios";
+import address from "./address";
+
+const getSongs = async (obj) => {
+  let endpoint = `${address}/songs`;
+  const queryArray = [];
+  if (obj) {
+    if (obj.name) {
+      queryArray.push(`name=${obj.name}`);
+      if (obj.exact) {
+        queryArray.push("exact=true");
+      }
+    }
+    if (obj.limit) {
+      queryArray.push(`limit=${obj.limit}`);
+    }
+    if (queryArray.length > 0) {
+      const query = queryArray.join("&");
+      endpoint = `${endpoint}?${query}`;
+    }
+  }
+
+  try {
+    const response = await axios.get(endpoint);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+};
+
+export default getSongs;
