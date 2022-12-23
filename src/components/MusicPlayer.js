373ae6ec714bc7d787b8f5b295e136f578e768ea
Perry Baran <perrybaran@gmail.com>
2022-12-23 13:11:12 +0000

    added icons and have music player show if no songs in playlist

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,17 +1,29 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
+import Playlist from "./Playlist";
 import "../styles/musicplayer.css";
+import {
+  music,
+  pause,
+  play,
+  skip,
+  volumeLow,
+  volumeMedium,
+  volumeMute,
+  menu,
+} from "../media/icons";
 
 const MusicPlayer = ({ playlist, playlistIndex, setPlaylistIndex }) => {
-  if (!playlist || playlist.length === 0) return null;
   const [playing, setPlaying] = useState(false);
   const [time, setTime] = useState(0);
   const [duration, setDuration] = useState(0);
   const [volume, setVolume] = useState(0.5);
+  const [showVolumeInput, setShowVolumeInput] = useState(false);
+  const [showPlaylist, setShowPlaylist] = useState(false);
   const audioRef = useRef(null);
 
-  const song = playlist[playlistIndex];
+  const song = playlist[playlistIndex] || {};
 
   const handlePlaying = (isPlaying) => {
     if (isPlaying === undefined) {
@@ -23,7 +35,7 @@ const MusicPlayer = ({ playlist, playlistIndex, setPlaylistIndex }) => {
 
   const skipSong = (forwards = true) => {
     if (forwards) {
-      if (playlistIndex === playlist.length) {
+      if (playlistIndex === playlist.length - 1) {
         setPlaylistIndex(0);
       } else {
         setPlaylistIndex((prev) => prev + 1);
@@ -31,7 +43,7 @@ const MusicPlayer = ({ playlist, playlistIndex, setPlaylistIndex }) => {
     } else if (playlistIndex > 0) {
       setPlaylistIndex((prev) => prev - 1);
     } else {
-      setPlaylistIndex(playlist.length);
+      setPlaylistIndex(playlist.length - 1);
     }
   };
 
@@ -77,6 +89,12 @@ const MusicPlayer = ({ playlist, playlistIndex, setPlaylistIndex }) => {
     }
   };
 
+  const volumeIcon = () => {
+    if (volume === 0) return volumeMute;
+    if (volume < 0.5) return volumeLow;
+    return volumeMedium;
+  };
+
   useEffect(() => {
     if (audioRef.current) {
       if (playing) {
@@ -109,11 +127,12 @@ const MusicPlayer = ({ playlist, playlistIndex, setPlaylistIndex }) => {
         src={song.audio}
         ref={audioRef}
         onLoadedMetadata={handleLoadedMetaData}
+        data-testid="audio"
       />
       <div className="music-player">
         <div className="music-player__left">
           <img
-            src={song.image}
+            src={song.image || music}
             alt={`${song.albumName} cover art`}
             className="music-player__cover-art"
           />
@@ -131,31 +150,75 @@ const MusicPlayer = ({ playlist, playlistIndex, setPlaylistIndex }) => {
             value={time}
             onChange={handleProgressBarChange}
             className="music-player__progress-bar"
+            data-testid="progressBar"
           />
           <div className="music-player__center__under">
             <p>{calcTime(time)}</p>
             <div>
-              <button type="button" onClick={() => skipSong(false)}>
-                Back
+              <button
+                type="button"
+                onClick={() => skipSong(false)}
+                className="music-player__skip-backwards"
+              >
+                <img src={skip} alt="skip backwards" />
               </button>
-              <button type="button" onClick={() => handlePlaying()}>
-                Play/Pause
+              <button
+                type="button"
+                onClick={() => handlePlaying()}
+                className="music-player__play-pause"
+              >
+                <img
+                  src={playing ? pause : play}
+                  alt={playing ? "play" : "pause"}
+                />
               </button>
-              <button type="button" onClick={() => skipSong()}>
-                Forwards
+              <button
+                type="button"
+                onClick={() => skipSong()}
+                className="music-player__skip-forwards"
+              >
+                <img src={skip} alt="skip forwards" />
               </button>
             </div>
             <p>{calcTime(duration)}</p>
           </div>
         </div>
-        <input
-          type="range"
-          onChange={handleVolume}
-          value={volume * 100}
-          min={0}
-          max={100}
-          name="volume"
-        />
+        <div className="music-player__right">
+          <div className="music-player__volume">
+            {showVolumeInput && (
+              <input
+                type="range"
+                onChange={handleVolume}
+                value={volume * 100}
+                min={0}
+                max={100}
+                name="volume"
+                data-testid="volume"
+              />
+            )}
+            <button
+              type="button"
+              onClick={() => setShowVolumeInput((prev) => !prev)}
+            >
+              <img src={volumeIcon()} alt="volume" />
+            </button>
+          </div>
+          <div className="music-player__playlist">
+            {showPlaylist && (
+              <Playlist
+                playlist={playlist}
+                playlistIndex={playlistIndex}
+                setPlaylistIndex={setPlaylistIndex}
+              />
+            )}
+            <button
+              type="button"
+              onClick={() => setShowPlaylist((prev) => !prev)}
+            >
+              <img src={menu} alt="playlist" />
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
