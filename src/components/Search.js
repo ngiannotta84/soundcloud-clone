6517b456d2a15b30dab55ecf831c1d1ca5ad5e4a
Perry Baran <perrybaran@gmail.com>
2022-12-23 15:49:56 +0000

    added ability to search for songs and artists as well as albums, ordered by more recent to latest

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,41 +2,70 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 import Album from "./Album";
+import Song from "./Song";
+import Artist from "./Artist";
 import getAlbums from "../requests/getAlbums";
+import getSongs from "../requests/getSongs";
+import getUsers from "../requests/getUsers";
 
 const Search = ({ handleSetPlaylist }) => {
   const { name } = useParams();
-  const [albums, setAlbums] = useState([]);
+  const [search, setSearch] = useState([]);
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await getAlbums({ name });
-        if (response) {
-          setAlbums(response);
-        } else {
-          setAlbums([]);
-        }
+        const albumResponse = await getAlbums({ name });
+        const songResponse = await getSongs({ name });
+        const artistResponse = await getUsers({ name });
+        const data = [...albumResponse, ...songResponse, ...artistResponse];
+        data.sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt));
+        setSearch(data);
       } catch (err) {
-        setAlbums([]);
+        setSearch([]);
       }
     })();
   }, [name]);
 
   return (
     <div>
-      {albums.length > 0 ? (
-        albums.map((album) => {
-          return (
-            <Album
-              artistName={album.User.name}
-              albumName={album.name}
-              albumArt={album.url}
-              songs={album.Songs}
-              key={album.id}
-              handleSetPlaylist={handleSetPlaylist}
-            />
-          );
+      {search.length > 0 ? (
+        search.map((data) => {
+          if (data.AlbumId) {
+            return (
+              <Song
+                artistName={data.Album.User.name}
+                albumName={data.Album.name}
+                albumArt={data.Album.url}
+                songName={data.name}
+                songAudio={data.url}
+                handleSetPlaylist={handleSetPlaylist}
+                key={`${data.id}${data.name}`}
+              />
+            );
+          }
+          if (data.UserId) {
+            return (
+              <Album
+                artistName={data.User.name}
+                albumName={data.name}
+                albumArt={data.url}
+                songs={data.Songs}
+                handleSetPlaylist={handleSetPlaylist}
+                key={`${data.id}${data.name}`}
+              />
+            );
+          }
+          if (data.email) {
+            return (
+              <Artist
+                name={data.name}
+                image={data.Albums[0].url}
+                key={`${data.id}${data.name}`}
+              />
+            );
+          }
+          return null;
         })
       ) : (
         <h2>No results</h2>
