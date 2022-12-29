315f14c71cbaadf4c663928fbd65dcc0a147b7b6
Perry Baran <perrybaran@gmail.com>
2022-12-29 15:44:39 +0000

    changed address to render hosted server

--- src/requests/instance.js
+++ src/requests/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const address = "https://soundcloud-clone-api.onrender.com";
-const address = "http://localhost:4000";
+const address = "https://soundcloud-clone-api.onrender.com";
+// const address = "http://localhost:4000";
 
 const instance = axios.create({
   withCredentials: true,
