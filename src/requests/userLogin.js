5c117e5f54ce4863a27c88792b65dfe76a288713
Perry Baran <perrybaran@gmail.com>
2022-12-29 13:16:29 +0000

    cookie is now set on the front-end instead of from the backend to avoid weird error

--- src/requests/userLogin.js
+++ src/requests/userLogin.js
@@ -1,8 +1,10 @@
+import Cookies from "js-cookie";
 import { postRequest } from "./helpers";
 
 const userLogin = async (data) => {
   try {
     const response = await postRequest("users/login", data);
+    Cookies.set("userToken", response.userToken, { expires: 1 });
     return response;
   } catch (err) {
     throw new Error(err);
--- src/requests/userSignup.js
+++ src/requests/userSignup.js
@@ -1,8 +1,10 @@
+import Cookies from "js-cookie";
 import { postRequest } from "./helpers";
 
 const userSignup = async (data) => {
   try {
     const response = await postRequest("users/signup", data);
+    Cookies.set("userToken", response.userToken, { expires: 1 });
     return response;
   } catch (err) {
     throw new Error(err);
