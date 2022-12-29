bbd17e73d025f07f2ad5dbbd2fad249fa3531276
Perry Baran <perrybaran@gmail.com>
2022-12-29 13:15:35 +0000

    cleaning up

--- src/tests/Playlist.test.js
+++ src/tests/Playlist.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Playlist from "../components/Playlist";
 import fakePlaylist from "./testData/fakePlaylist";
 
