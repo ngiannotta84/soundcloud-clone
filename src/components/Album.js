45d62a02f6621e9af76b216e4ff76076afdea70c
Perry Baran <perrybaran@gmail.com>
2023-01-04 16:07:04 +0000

    styling album + can add all of album to playlist

--- src/components/Album.js
+++ src/components/Album.js
@@ -18,15 +18,35 @@ const Album = ({
     (a, b) => Number(a.position) - Number(b.position)
   );
 
-  const handleClick = (song, addNext = false) => {
-    const data = {
+  const songData = (song) => {
+    return {
       image: albumArt,
       artistName,
       albumName,
       songName: song.name,
       audio: song.url,
     };
+  };
+
+  const handleClickSong = (song, addNext = false) => {
+    const data = songData(song);
     handleSetPlaylist(data, addNext);
+  };
+
+  const handleClickAlbum = (addNext = false) => {
+    const { length } = orderedSongs;
+
+    if (addNext) {
+      for (let i = length - 1; i >= 0; i -= 1) {
+        const data = songData(orderedSongs[i]);
+        handleSetPlaylist(data, addNext);
+      }
+    } else {
+      for (let i = 0; i < length; i += 1) {
+        const data = songData(orderedSongs[i]);
+        handleSetPlaylist(data, addNext);
+      }
+    }
   };
 
   return (
@@ -37,41 +57,59 @@ const Album = ({
           alt={`${albumName} cover art`}
           className="album__cover-art"
         />
-        <div>
+        <div className="album__info">
           <h2 className="album__artist-name">
             <Link to={`/profile/${artistName}`}>{artistName}</Link>
           </h2>
           <h3 className="album__album-name">{albumName}</h3>
         </div>
+        <div className="album__buttons">
+          <button
+            type="button"
+            className="album__buttons__button"
+            onClick={() => handleClickAlbum()}
+          >
+            Add Album to queue
+          </button>
+          <button
+            type="button"
+            className="album__buttons__button"
+            onClick={() => handleClickAlbum(true)}
+          >
+            Play Album next
+          </button>
+          {userId !== null && userId === albumUserId && (
+            <Link to={`/edit/${albumId}`} className="album__buttons__link">
+              Edit
+            </Link>
+          )}
+        </div>
       </div>
-      <div>
+      <li className="album__songs">
         {orderedSongs.map((song) => {
           return (
-            <div key={song.id} className="album__song">
+            <ul key={song.id} className="album__song">
               <h4 className="album__song__song-name">{song.name}</h4>
               <div>
                 <button
                   type="button"
-                  className="album__song__add-song"
-                  onClick={() => handleClick(song)}
+                  className="album__song__button"
+                  onClick={() => handleClickSong(song)}
                 >
                   Add to queue
                 </button>
                 <button
                   type="button"
-                  className="album__song__add-song"
-                  onClick={() => handleClick(song, true)}
+                  className="album__song__button"
+                  onClick={() => handleClickSong(song, true)}
                 >
                   Play next
                 </button>
               </div>
-            </div>
+            </ul>
           );
         })}
-      </div>
-      {userId !== null && userId === albumUserId && (
-        <Link to={`/edit/${albumId}`}>Edit</Link>
-      )}
+      </li>
     </div>
   );
 };
